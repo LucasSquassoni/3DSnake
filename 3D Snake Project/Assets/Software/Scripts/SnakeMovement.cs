dacf2ac936d24a4fc9881c8a088b743a4e6442a0
Lucas Squassoni <work.lucassq@gmail.com>
2019-01-04 19:47:29 +1100

    Added first iteration of movement

---
+++ 3D Snake Project/Assets/Software/Scripts/SnakeMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+    Vector3 dir = Vector3.right;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(MovementTick());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void FixedUpdate() {
+        ChangeDir();
+    }
+
+    void ConstantMovement() {
+        transform.position += dir;
+    }
+
+    void ChangeDir() {
+        if (Input.GetAxisRaw("Vertical") == 1 && dir != Vector3.back)
+            StartCoroutine(directionCoroutine(Vector3.forward));
+        else if (Input.GetAxisRaw("Vertical") == -1 && dir != Vector3.forward)
+            StartCoroutine(directionCoroutine(Vector3.back));
+        else if (Input.GetAxisRaw("Horizontal") == -1 && dir != Vector3.right)
+            StartCoroutine(directionCoroutine(Vector3.left));
+        else if (Input.GetAxisRaw("Horizontal") == 1 && dir != Vector3.left)
+            StartCoroutine(directionCoroutine(Vector3.right));
+    }
+
+    IEnumerator MovementTick() {
+        while (true) {
+            yield return new WaitForSeconds(0.3f);
+            ConstantMovement();
+        }
+    }
+
+    IEnumerator directionCoroutine(Vector3 direction) {
+        dir = direction;
+        yield return new WaitForSeconds(0.3f);
+    }
+}
